7556161d723b5447cbefba573be3897618dea2ae
kehaus <kehaus@protonmail.com>
2022-03-02 21:55:21 -0500

    add install_required to setup.py for numpy and pandas packages

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='rhk-sm4',
     version='0.1.0',
-    packages=find_packages(include=['rhk_sm4', 'rhk_sm4.*'])
+    packages=find_packages(include=['rhk_sm4', 'rhk_sm4.*']),
+    install_requires=[
+        'numpy',
+        'pandas'
+    ]
 )
 
